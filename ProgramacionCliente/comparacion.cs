56b35a1f77ff43d1df20682029cc4c02853fedeb
Jocelyn Zamora <92815037+JocelynBZamora@users.noreply.github.com>
2024-12-13 19:37:26 -0600

    Agregar
    Trabajos realizados en programación lado del cliente

---
+++ ProgramacionCliente/comparacion.cs
@@ -0,0 +1,12 @@
+int numero1, numero2;
+console.writeline("Escriba un numero: ");
+console.readline(numero1);
+console.writeline("Escriba el segundo numero: ");
+console.readline(numero2);
+
+if(numero1 == numero2){
+    console.writeline("los numeros son iguales");
+}
+else{
+    console.writeline("los numeros son diferentes")
+}
